4677e28a81411c1b64cc2fb7c45c65aba6316856
Mo Cheney <morrison.cheney@gmail.com>
2026-02-05 09:25:18 -0700

    Added some experimental functions.

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -54,12 +55,43 @@ public class ChessGame {
      */
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
         ChessPiece p = this.board.getPiece(startPosition);
-        if (p != null){
-            // THIS NEEDS TO LEVEL UP A TON
-            return p.pieceMoves(this.board, startPosition);
-        } else {
-            return null;
+
+        ArrayList<ChessMove> moves = new ArrayList<>();
+
+        ArrayList<ChessMove> possibilities = new ArrayList<>();
+        for (int r = 1; r < 9; r++) {
+            for (int c = 1; c < 9; c++) {
+                if (p != null && p.color == this.currentTurn) {
+                    possibilities.addAll(p.pieceMoves(this.board, startPosition));
+                } else {
+                    return null;
+                }
+
+
+
+            }
         }
+
+        // Iterate through each move
+        for (ChessMove move: possibilities) {
+            ChessBoard boardCopy = this.board;
+            // pretend to execute the given move
+            boardCopy = executeMove(boardCopy, move);
+            // if the color who's moving's king does not end in check
+            // add move
+            // else
+            // continue
+        }
+        return moves;
+    }
+
+
+    // create a function called checkForCheck(ChessBoard checkBoard){}
+    // which returns the color of the king in check or null
+
+    private TeamColor checkForCheck(ChessBoard checkBoard, ChessMove move) {
+
+        return TeamColor.BLACK;
     }
 
     /**
@@ -69,7 +101,29 @@ public class ChessGame {
      * @throws InvalidMoveException if move is invalid
      */
     public void makeMove(ChessMove move) throws InvalidMoveException {
-        throw new RuntimeException("Not implemented");
+
+    }
+
+    /**
+     *
+     * @param currBoard
+     * @param move
+     * @return currBoard with move executed
+     */
+    private ChessBoard executeMove(ChessBoard currBoard, ChessMove move){
+        ChessPiece piece = currBoard.getPiece(move.getStartPosition());
+        currBoard.addPiece(move.getStartPosition(), null);
+        if(move.getPromotionPiece() != null) {
+            currBoard.addPiece(move.getEndPosition(), piece);
+        } else {
+            currBoard.addPiece(move.getEndPosition(), new ChessPiece(piece.getTeamColor(), move.getPromotionPiece()));
+        }
+        return currBoard;
+    }
+
+    private boolean iterativeCheckFinder(ChessBoard checkBoard, TeamColor color){
+        // Checks if the color king is in check.
+        return false;
     }
 
     /**
@@ -79,7 +133,32 @@ public class ChessGame {
      * @return True if the specified team is in check
      */
     public boolean isInCheck(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+//        throw new RuntimeException("Not implemented");
+        ChessPiece currPiece;
+        ArrayList<ChessMove> opMoves = new ArrayList<>();
+        ChessPosition currPos;
+        ChessPosition kingPos = null;
+        for (int row = 1; row < 9; row += 1) {
+            for (int col = 1; col < 9; col += 1) {
+                currPos = new ChessPosition(row, col);
+                currPiece = this.board.getPiece(currPos);
+                if (currPiece != null) {
+                    if (currPiece.getTeamColor() == teamColor && currPiece.getPieceType() == ChessPiece.PieceType.KING){
+                        kingPos = currPos;
+                    }
+                    if (currPiece.getTeamColor() != teamColor){
+                        opMoves.addAll(validMoves(currPos));
+                    }
+                }
+            }
+        }
+
+        for (ChessMove move: opMoves){
+            if (move.getEndPosition() == kingPos) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
