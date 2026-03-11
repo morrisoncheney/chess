e227cc01ee30f48ae0c7e7258a9fda8afc2a192c
Mo Cheney <morrison.cheney@gmail.com>
2026-03-11 09:53:43 -0600

    Code quality corrections. Still need one more commit. That won't be a problem because I still have to write a lot of tests.

--- server/src/main/java/dataaccess/MySqlDataAccess.java
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -262,10 +262,10 @@ private final String[] createStatements = {
             try (PreparedStatement ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
                 for (int i = 0; i < params.length; i++) {
                     Object param = params[i];
-                    if (param instanceof String p) ps.setString(i + 1, p);
-                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
-                    else if (param instanceof GameData g) ps.setString(i + 1, g.toString());
-                    else if (param == null) ps.setNull(i + 1, NULL);
+                    if (param instanceof String p) { ps.setString(i + 1, p); }
+                    else if (param instanceof Integer p) { ps.setInt(i + 1, p); }
+                    else if (param instanceof GameData g) { ps.setString(i + 1, g.toString()); }
+                    else if (param == null) { ps.setNull(i + 1, NULL); }
                 }
                 ps.executeUpdate();
 
