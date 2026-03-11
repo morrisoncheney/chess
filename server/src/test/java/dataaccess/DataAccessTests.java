a1abd70595abf89363f6110ac63f1cdff55be4ee
Mo Cheney <morrison.cheney@gmail.com>
2026-03-11 15:01:33 -0600

    Had to throw my boy Austin in there some more.

--- server/src/test/java/dataaccess/DataAccessTests.java
+++ server/src/test/java/dataaccess/DataAccessTests.java
@@ -1,5 +1,6 @@
 package dataaccess;
 
+import model.AuthData;
 import model.UserData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,11 +10,13 @@ import org.junit.jupiter.api.Test;
 public class DataAccessTests {
     // we shall begin testing
     static MySqlDataAccess db;
+    static UserData unfindable;
 
     @BeforeAll
     static void setup() {
         db = new MySqlDataAccess();
         db.deleteAllUserData();
+        unfindable = new UserData("Austin Powers", "shallweshagnoworshaglater", "groovy_baby_1967@sis.gov");
     }
 
     @Test
@@ -28,19 +31,7 @@ public class DataAccessTests {
     }
 
     @Test
-    void getUserPass() {
-        UserData user = new UserData("me1", "memememe1", "me@me.me1");
-
-        db.addUser(user);
-
-        UserData rec = db.getUser(user.username());
-
-        Assertions.assertEquals(user.username(), rec.username());
-    }
-
-    @Test
     void getInternationalManOfMystery(){
-        UserData unfindable = new UserData("Austin Powers", "shallweshagnoworshaglater", "groovy_baby_1967@sis.gov");
 
         UserData rec = db.getUser(unfindable.username());
 
@@ -64,6 +55,41 @@ public class DataAccessTests {
         Assertions.assertNull(rec);
     }
 
+    @Test
+    void addAndGetAuthTest(){
+        UserData user = new UserData("me1", "memememe1", "me@me.me1");
 
+        db.addUser(user);
+
+        AuthData auth = new AuthData("me1", "67");
+
+        db.addAuth(auth);
+
+        AuthData rec = db.getAuth(auth.username());
+
+        Assertions.assertEquals(auth, rec);
+    }
+
+    @Test
+    void getDeletedAuthTest(){
+        UserData user = new UserData("me1", "memememe1", "me@me.me1");
+
+        db.addUser(user);
+
+        AuthData auth = new AuthData("me1", "67");
+
+        db.addAuth(auth);
+
+        db.deleteAuth(auth.authToken());
+
+        AuthData rec = db.getAuth(auth.authToken());
+
+        Assertions.assertNull(rec);
+    }
+
+    @Test
+    void deleteNonexistentAuth(){
+        db.getAuth("1967_baby_yeah");
+    }
 
 }
