ebb6a2db139c14a0776ff420610a58b05ab4f83a
Mo Cheney <morrison.cheney@gmail.com>
2026-03-14 22:48:26 -0600

    Trying to get a nice 8x8 chess board shape going.

--- client/src/main/java/ui/tictactoe.java
+++ client/src/main/java/ui/tictactoe.java
@@ -10,8 +10,8 @@ public class tictactoe {
 
     // Board dimensions.
     private static final int BOARD_SIZE_IN_SQUARES = 8;
-    private static final int SQUARE_SIZE_IN_PADDED_CHARS = 2;
-    private static final int LINE_WIDTH_IN_PADDED_CHARS = 1;
+    private static final int SQUARE_SIZE_IN_PADDED_CHARS = 3;
+    private static final int LINE_WIDTH_IN_PADDED_CHARS = 0;
 
     // Padded characters.
     private static final String EMPTY = "   ";
@@ -22,110 +22,117 @@ public class tictactoe {
 
 
     public static void main(String[] args) {
-        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8); // needed to print together
 
         out.print(ERASE_SCREEN);
 
-        drawHeaders(out);
+//        drawHeaders(out);
 
         drawTicTacToeBoard(out);
 
-        out.print(SET_BG_COLOR_BLACK);
-        out.print(SET_TEXT_COLOR_WHITE);
+//        out.print(SET_BG_COLOR_BLACK);
+//        out.print(SET_TEXT_COLOR_WHITE);
     }
 
-    private static void drawHeaders(PrintStream out) {
+//    private static void drawHeaders(PrintStream out) {
+//
+//        setBlack(out);
+//
+//
+//        for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
+//
+//
+//            if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
+//                out.print(EMPTY.repeat(LINE_WIDTH_IN_PADDED_CHARS));
+//            }
+//        }
+//
+//        out.println();
+//    }
 
-        setBlack(out);
+//    private static void drawHeader(PrintStream out, String headerText) {
+//        int prefixLength = SQUARE_SIZE_IN_PADDED_CHARS / 2;
+//        int suffixLength = SQUARE_SIZE_IN_PADDED_CHARS - prefixLength - 1;
+//
+//        out.print(EMPTY.repeat(prefixLength));
+//        printHeaderText(out, headerText);
+//        out.print(EMPTY.repeat(suffixLength));
+//    }
 
-//        String[] headers = { "TIC", "TAC", "TOE" };
-        for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
-//            drawHeader(out, headers[boardCol]);
-
-            if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
-                out.print(EMPTY.repeat(LINE_WIDTH_IN_PADDED_CHARS));
-            }
-        }
-
-        out.println();
-    }
-
-    private static void drawHeader(PrintStream out, String headerText) {
-        int prefixLength = SQUARE_SIZE_IN_PADDED_CHARS / 2;
-        int suffixLength = SQUARE_SIZE_IN_PADDED_CHARS - prefixLength - 1;
-
-        out.print(EMPTY.repeat(prefixLength));
-        printHeaderText(out, headerText);
-        out.print(EMPTY.repeat(suffixLength));
-    }
-
-    private static void printHeaderText(PrintStream out, String player) {
-        out.print(SET_BG_COLOR_BLACK);
-        out.print(SET_TEXT_COLOR_GREEN);
-
-        out.print(player);
-
-        setBlack(out);
-    }
+//    private static void printHeaderText(PrintStream out, String player) {
+//        out.print(SET_BG_COLOR_BLACK);
+//        out.print(SET_TEXT_COLOR_GREEN);
+//
+//        out.print(player);
+//
+//        setBlack(out);
+//    }
 
     private static void drawTicTacToeBoard(PrintStream out) {
 
         for (int boardRow = 0; boardRow < BOARD_SIZE_IN_SQUARES; ++boardRow) {
 
-            drawRowOfSquares(out);
+            drawRowOfSquares(out, boardRow);
 
-            if (boardRow < BOARD_SIZE_IN_SQUARES - 1) {
-                // Draw horizontal row separator.
-                drawHorizontalLine(out);
-                setBlack(out);
-            }
+//            if (boardRow < BOARD_SIZE_IN_SQUARES - 1) {
+//                // Draw horizontal row separator.
+//                drawHorizontalLine(out);
+//                setBlack(out);
+//            }
         }
     }
 
-    private static void drawRowOfSquares(PrintStream out) {
+    private static void drawRowOfSquares(PrintStream out, int boardRow) {
 
         for (int squareRow = 0; squareRow < SQUARE_SIZE_IN_PADDED_CHARS; ++squareRow) {
             for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
-                setWhite(out);
 
-                if (squareRow == SQUARE_SIZE_IN_PADDED_CHARS / 2) {
-                    int prefixLength = SQUARE_SIZE_IN_PADDED_CHARS / 2;
-                    int suffixLength = SQUARE_SIZE_IN_PADDED_CHARS - prefixLength - 1;
 
-                    out.print(EMPTY.repeat(prefixLength));
-                    printPlayer(out, rand.nextBoolean() ? X : O);
-                    out.print(EMPTY.repeat(suffixLength));
+                if (boardRow + boardCol % 2 == 0) {
+                    setBlack(out);
+                } else {
+                    setWhite(out);
                 }
-                else {
+
+//                if (squareRow == SQUARE_SIZE_IN_PADDED_CHARS / 2) {
+//                    int prefixLength = SQUARE_SIZE_IN_PADDED_CHARS / 2;
+//                    int suffixLength = SQUARE_SIZE_IN_PADDED_CHARS - prefixLength - 1;
+//
+//                    out.print(EMPTY.repeat(prefixLength));
+//                    printPlayer(out, rand.nextBoolean() ? X : O);
+//                    out.print(EMPTY.repeat(suffixLength));
+//                }
+//                else {
                     out.print(EMPTY.repeat(SQUARE_SIZE_IN_PADDED_CHARS));
-                }
+//                }
 
-                if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
-                    // Draw vertical column separator.
-                    setRed(out);
-                    out.print(EMPTY.repeat(LINE_WIDTH_IN_PADDED_CHARS));
-                }
+//                if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
+//                    // Draw vertical column separator.
+//                    setRed(out);
+//                    out.print(EMPTY.repeat(LINE_WIDTH_IN_PADDED_CHARS));
+//                }
 
-                setBlack(out);
+//                setBlack(out);
+                out.print(RESET_BG_COLOR);
             }
 
             out.println();
         }
     }
 
-    private static void drawHorizontalLine(PrintStream out) {
-
-        int boardSizeInSpaces = BOARD_SIZE_IN_SQUARES * SQUARE_SIZE_IN_PADDED_CHARS +
-                (BOARD_SIZE_IN_SQUARES - 1) * LINE_WIDTH_IN_PADDED_CHARS;
-
-        for (int lineRow = 0; lineRow < LINE_WIDTH_IN_PADDED_CHARS; ++lineRow) {
-            setRed(out);
-            out.print(EMPTY.repeat(boardSizeInSpaces));
-
-            setBlack(out);
-            out.println();
-        }
-    }
+//    private static void drawHorizontalLine(PrintStream out) {
+//
+//        int boardSizeInSpaces = BOARD_SIZE_IN_SQUARES * SQUARE_SIZE_IN_PADDED_CHARS +
+//                (BOARD_SIZE_IN_SQUARES - 1) * LINE_WIDTH_IN_PADDED_CHARS;
+//
+//        for (int lineRow = 0; lineRow < LINE_WIDTH_IN_PADDED_CHARS; ++lineRow) {
+//            setRed(out);
+//            out.print(EMPTY.repeat(boardSizeInSpaces));
+//
+//            setBlack(out);
+//            out.println();
+//        }
+//    }
 
     private static void setWhite(PrintStream out) {
         out.print(SET_BG_COLOR_WHITE);
@@ -142,12 +149,12 @@ public class tictactoe {
         out.print(SET_TEXT_COLOR_BLACK);
     }
 
-    private static void printPlayer(PrintStream out, String player) {
-        out.print(SET_BG_COLOR_WHITE);
-        out.print(SET_TEXT_COLOR_BLACK);
-
-        out.print(player);
-
-        setWhite(out);
-    }
+//    private static void printPlayer(PrintStream out, String player) {
+//        out.print(SET_BG_COLOR_WHITE);
+//        out.print(SET_TEXT_COLOR_BLACK);
+//
+//        out.print(player);
+//
+//        setWhite(out);
+//    }
 }
